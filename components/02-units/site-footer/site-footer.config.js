3465a6b900c878a2ba4bd91d96cfb0490ba3207f
Graham Smith <g@blaxian.com>
2016-10-24 14:24:43 +0100

    add the site footer component

---
+++ components/02-units/site-footer/site-footer.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    "status": "wip",
+
+    "context": {
+        "links": [
+            { "title": "Test Lab", "url": "#" },
+            { "title": "Accessibility", "url": "#" },
+            { "title": "Cookies", "url": "#" },
+            { "title": "Styleguide", "url": "#" },
+            { "title": "Jobs", "url": "#" }
+        ],
+        "message": "Lovingly crafted by Clearleft in Brighton, England",
+        "small": "&copy; 2005‑now Clearleft Ltd. All rights reserved."
+    }
+}
