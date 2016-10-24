560be0d40fdc4dff8bd130497b2b726196044810
lottejackson <lottejackson@yahoo.co.uk>
2016-10-24 13:41:59 +0100

    Add content variables to the component

---
+++ components/02-units/details/details.config.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = {
+    "status": "wip",
+    "collated": true,
+    "context": {
+        "title": "Our users",
+        "details": [{
+            "detailItem": "Price",
+            "detailDescription": "£2500"
+        },{
+            "detailItem": "Attendees",
+            "detailDescription": "Up to 8 people"
+        },{
+            "detailItem": "Duration",
+            "detailDescription": "1 day"
+        },{
+            "detailItem": "Book now",
+            "detailDescription": "Call Andy on +44 (0)845 858 6563"
+        }]
+    }
+}
