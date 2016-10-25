6546dd1b417bba2ad860a4b2181ed6a980aadced
Graham Smith <g@blaxian.com>
2016-10-25 12:07:47 +0100

    Splits the masthead up into its constituent components - Logo component created - Primary Navigation component created

---
+++ components/02-units/primary-navigation/primary-navigation.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    "status": "wip",
+    "context": {
+        "links": [
+            { "title": "About Us", "url": "#" },
+            { "title": "Our Work", "url": "#" },
+            { "title": "Our Approach", "url": "#" },
+            { "title": "Our Events", "url": "#" },
+            { "title": "Thinks", "url": "#" },
+            { "title": "Contact", "url": "#" }
+        ]
+    }
+}
