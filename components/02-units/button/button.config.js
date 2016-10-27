5e3f1e12e4607645eae005b9285de4887d328d31
Graham Smith <g@blaxian.com>
2016-10-27 16:04:39 +0100

    Tweaks the base typography system and updates all components - Removed the pixel font size on the html element and added to body instead. This allows pages to adapt to a user's browser font size prefs - Added some px to rem/em conversion functions - Line height is now relative to the components font size, not the base font size

--- components/02-units/button/button.config.js
+++ components/02-units/button/button.config.js
@@ -1,7 +1,8 @@
-'use strict';
+
 module.exports = {
     "status": "wip",
     "collated": true,
+    "preview": "@preview-dark",
     "context": {
         "buttonText": "Check out our open positions &#10141;"
     }
