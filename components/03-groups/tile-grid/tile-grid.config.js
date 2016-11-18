14186d6760ac8c5b3bd794bc9614935a281bf8ea
lottejackson <lottejackson@yahoo.co.uk>
2016-11-18 17:53:43 +0000

    First pass at new team grid

--- components/03-groups/tile-grid/tile-grid.config.js
+++ components/03-groups/tile-grid/tile-grid.config.js
@@ -1,23 +1,19 @@
 'use strict';
-const faker     = require('faker');
-const persons   = [];
+var faker = require('faker');
+var persons = [];
 var imageSrc = ["/assets/img/team/james-g.jpg", "/assets/img/team/ben-w.jpg", "/assets/img/team/ellen.jpg", "/assets/img/team/hana.jpg", "/assets/img/team/charlotte.jpg", "/assets/img/team/andy-t.jpg"];
-for(var i = 0; i < 26; i++){
-    var number = Math.floor((Math.random() * 5) + 1);
+for(var i = 0; i < 20; i++){
+    var number = Math.floor(Math.random() * imageSrc.length);
     persons.push({
         indexCardName: faker.name.findName(),
         indexCardInfo: faker.name.jobTitle(),
         imageSrc: imageSrc[number]
     });
 }
-
 module.exports = {
     "status": "wip",
     "context": {
-        "links": [{ title: "Home", url: "#" }],
-        "mastheadTitle": "Meet the Clearlefties",
         "persons": persons,
-        "message": "Lovingly crafted by Clearleft in Brighton, England",
-        "small": "&copy; 2005‑now Clearleft Ltd. All rights reserved."
+        "imageSrc": "@index-card.imageSrc"
     }
 };
