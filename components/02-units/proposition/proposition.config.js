338e79100fdbc0a6b698cd8a99d692edf5547e73
lottejackson <lottejackson@yahoo.co.uk>
2016-11-25 11:17:41 +0000

    Change signpost to proposition and rebuild

---
+++ components/02-units/proposition/proposition.config.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = {
+    "status": "wip",
+    "context": {
+        "signpostTitle": "Create empathy maps and user journeys",
+        "signpostLink": "About this workshop ➞",
+        "signpostModifier": "--bgGreen",
+        "propositionLogos": [
+            { "src": "/assets/img/logos/wellcome.png" },
+            { "src": "/assets/img/logos/penguin.png" },
+            { "src": "/assets/img/logos/johnlewis.png" },
+            { "src": "/assets/img/logos/wellcome.png" }
+        ]
+    }
+}
---
+++ components/03-groups/signpost-shape/signpost-shape.config.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = {
+    "status": "wip",
+    "context": {
+        "signpostTitle": "Create empathy maps and user journeys",
+        "signpostLink": "About this workshop ➞",
+        "signpostModifier": "@proposition.signpostModifier",
+        "propositionLogos": "@proposition.propositionLogos"
+    }
+}
