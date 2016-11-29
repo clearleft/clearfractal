004e5eca33aab0b49259546c2d1247f6443d8090
lottejackson <lottejackson@yahoo.co.uk>
2016-11-29 12:14:42 +0000

    Go back to specific footer icons and remove social media modifier

---
+++ components/02-units/social-media/social-media.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    "status": "wip",
+    "context": {
+        "socialLinks": [
+            { "src": "http://fillmurray.com/30/30", "url": "#" },
+            { "src": "http://fillmurray.com/30/30", "url": "#" },
+            { "src": "http://fillmurray.com/30/30", "url": "#" }
+        ]
+    }
+}
