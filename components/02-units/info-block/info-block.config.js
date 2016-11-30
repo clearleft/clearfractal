4c0f982d87ce619f9b89f25764208ee978cb7172
lottejackson <lottejackson@yahoo.co.uk>
2016-11-30 15:51:15 +0000

    Add info block to a group and then the team member page

--- components/02-units/info-block/info-block.config.js
+++ components/02-units/info-block/info-block.config.js
@@ -2,7 +2,6 @@
 module.exports = {
     "status": "wip",
     "context": {
-        "infoBlockHeading": "Published Books",
         "infoBlockContents": [
             { "title": "Web Accessibility: Web Standards and Regulatory Compliance", "sub": "published by Friends of Ed", "url": "#", "src": "https://www.fillmurray.com/60/72" },
             { "title": "HTML5 for Web Designers", "sub": "published by Friends of Ed", "url": "#", "src": "https://www.fillmurray.com/60/72" },
@@ -13,9 +12,8 @@ module.exports = {
     },
     "variants": [
         {
-            "name": "Without images",
+            "name": "without-images",
             "context": {
-                "infoBlockHeading": "Previous Talks",
                 "infoBlockContents": [
                     { "title": "Frontier Conference 2016", "sub": "16th November 2016", "url": "#" },
                     { "title": "London CSS", "sub": "30th November 2016", "url": "#" },
