945315c0e8558e2c35b953512020629cbafc6a35
lottejackson <lottejackson@yahoo.co.uk>
2016-12-01 16:06:50 +0000

    Add modifier for field border

--- components/02-units/input-group/input-group.config.js
+++ components/02-units/input-group/input-group.config.js
@@ -1,9 +1,15 @@
 'use strict';
 module.exports = {
     "status": "wip",
-    "collated": true,
     "context": {
         "inputLabel": "Email address",
         "inputID": "ID1"
-    }
+    },
+    "variants": [
+    {
+        "name": "encased",
+        "context": {
+            "inputModifier": "--encased"  
+        }
+    }]
 };
