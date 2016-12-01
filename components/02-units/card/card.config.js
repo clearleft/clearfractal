8ef0bc3c90846dff82d630b864f2bfcd917090ff
lottejackson <lottejackson@yahoo.co.uk>
2016-12-01 09:57:38 +0000

    Rebuild default card

--- components/02-units/card/card.config.js
+++ components/02-units/card/card.config.js
@@ -1,10 +1,8 @@
 'use strict';
 module.exports = {
     "status": "wip",
-    "collated": true,
     "context": {
         "imageSrc": "@index-card.imageSrc",
-        "closeContext": "2 weeks ago",
         "cardTitle": "Article Title Goes here",
         "openContext": "Category",
         "cardDescription": "In my previous article, I wrote about some of the misconceptions with “agile” that I see in organisations. This article will focus on one of them specifically – the common (mis)understanding that agile—and more recently lean start-up methods—are seen as operational [...]"
@@ -17,7 +15,6 @@ module.exports = {
             "cardTitle": "Article Title Goes here",
             "indexCard": true,
             "secondContext": "Visual design, 2 hours",
-            "closeContext": false,
             "cardDescription": false
         }
     },
@@ -28,7 +25,6 @@ module.exports = {
             "cardTitle": "The 20 second gut test",
             "indexCard": true,
             "secondContext": "Visual design, 2 hours",
-            "closeContext": false,
             "cardDescription": false
         }
     }]
