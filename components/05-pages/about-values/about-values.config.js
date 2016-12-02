ca8889c98786edd8e219b6be13dcb968f0b2f1d3
lottejackson <lottejackson@yahoo.co.uk>
2016-12-02 18:16:32 +0000

    Add breadcrumb option to masthead

--- components/05-pages/about-values/about-values.config.js
+++ components/05-pages/about-values/about-values.config.js
@@ -9,6 +9,7 @@ module.exports = {
         "primaryNavlinks": "@primary-navigation.primaryNavlinks",
         "mastheadModifier": "--streak",
         "mastheadTitle": "What we stand for &amp; believe in",
+        "mastheadBreadcrumb": true,
         "pageLeadModifier": "--pinch",
         "leadText": "Andy has been involved in digital in one form or another since 1999. Originally a front-end developer and visual designer, he naturally migrated towards the world of UX with a covert desire to seed the importance of design (with a big D) earlier in the conception and planning of projects.",
         "chapters": "@chapters.chapters",
