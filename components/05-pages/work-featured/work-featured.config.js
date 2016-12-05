928971a328db92ff010c784684773c116ce3ff1b
lottejackson <lottejackson@yahoo.co.uk>
2016-12-05 18:19:29 +0000

    Start featured and basic project landings

---
+++ components/05-pages/work-featured/work-featured.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let image = "/assets/img/team/andy-t-lg.jpg";
+
+module.exports = {
+    "title": "Team Member",
+    "status": "wip",
+    "context": {
+        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+        "mastheadModifier": "--streak",
+        "mastheadTitle": "What we stand for &amp; believe in",
+        "siteFooterSmall": "&copy; 2005‑now Clearleft Ltd. All rights reserved.",
+        "socialLinks": "@social-media.socialLinks"
+    }
+};
