dc8e9ff58ab87dacb05dcc7c5319a845b3842e68
lottejackson <lottejackson@yahoo.co.uk>
2016-12-09 17:40:59 +0000

    Add some featured case studies

--- components/02-units/showcase/showcase.config.js
+++ components/02-units/showcase/showcase.config.js
@@ -6,6 +6,29 @@ module.exports = {
         "showcaseTitle": "Changing the approach to digital publishing.",
         "showcaseText": "Clearleft worked with the Royal Borough of Kensington and Chelsea to fundamentally change their approach to digital publishing, resulting in hugely increased user satisfaction and a much easier internal workflow.",
         "siteFooterSmall": "&copy; 2005‑now Clearleft Ltd. All rights reserved.",
+        "showcaseImage": "/assets/img/work/rbkc/landing.png",
         "socialLinks": "@social-media.socialLinks"
-    }
+    },
+    "variants": [{
+        "name": "Featured Channel4",
+        "context": {
+            "showcaseModifier": "--featuredCh4",
+            "showcaseImage": "/assets/img/work/ch4/sling.png",
+            "showcaseTitle": "Take on Goliath.",
+            "showcaseText": "How could our small team make any impact on the giant eco-system of content in Channel 4’s News Room? This is a story of how small changes make a big difference.",
+        }
+    },
+    {
+        "name": "Featured RBKC",
+        "context": {
+            "showcaseModifier": "--featuredRBKC",
+            "showcaseImage": "/assets/img/work/penguin/penguin.png"
+        }
+    },
+    {
+        "name": "Featured Evo",
+        "context": {
+            "showcaseModifier": "--featuredEvo"
+        }
+    }]
 };
