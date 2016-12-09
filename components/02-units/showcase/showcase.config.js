43dc2691cafe34f83fbab17c4712bf62830705ea
lottejackson <lottejackson@yahoo.co.uk>
2016-12-09 11:05:21 +0000

    Add component for work showcase

---
+++ components/02-units/showcase/showcase.config.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = {
+    "status": "wip",
+    "context": {
+        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+        "showcaseTitle": "Changing the approach to digital publishing.",
+        "showcaseText": "Clearleft worked with the Royal Borough of Kensington and Chelsea to fundamentally change their approach to digital publishing, resulting in hugely increased user satisfaction and a much easier internal workflow.",
+        "siteFooterSmall": "&copy; 2005‑now Clearleft Ltd. All rights reserved.",
+        "socialLinks": "@social-media.socialLinks"
+    }
+};
