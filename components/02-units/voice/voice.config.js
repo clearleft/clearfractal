0c83c491902436dedfe6767fa33228a99f4639f8
lottejackson <lottejackson@yahoo.co.uk>
2016-12-12 19:39:23 +0000

    Move voice code into pattern

---
+++ components/02-units/voice/voice.config.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = {
+    "status": "wip",
+    "context": {
+        "voiceDescription": "We’re in the news room, in the centre of all the activity, watching the team capture, edit, and share events from all over the world.  It’s fast-moving, exciting, and iterative. In that moment, I realised that we could also design in a way that harnesses that same excitement.",
+        "voiceCite": "James Box, UX Director, Team lead"
+    },
+    "variants": [{
+        "name": "tall",
+        "context": {
+            "voiceModifier": "--tall"
+        }
+    }]
+};
--- components/05-pages/ch4-featured/ch4-featured.config.js
+++ components/05-pages/ch4-featured/ch4-featured.config.js
@@ -3,8 +3,14 @@ module.exports = {
     "status": "wip",
     "context": {
         "primaryNavlinks": "@primary-navigation.primaryNavlinks",
-        "featuredTitle": "Take on Goliath.",
-        "featuredText": "How could our small team make any impact on the giant eco-system of content in Channel 4’s News Room? This is a story of how small changes make a big difference.",
+        "showcaseModifier": "--featuredCh4",
+        "showcaseImage": "/assets/img/work/ch4/sling.png",
+        "showcaseLogo": "/assets/img/work/ch4/ch4-logo.svg",
+        "showcaseTitle": "Take on Goliath.",
+        "showcaseText": "How could our small team make any impact on the giant eco-system of content in Channel 4’s News Room? This is a story of how small changes make a big difference.",
+        "voiceModifier": "--tall",
+        "voiceDescription": "@voice.voiceDescription",
+        "voiceCite": "@voice.voiceCite",
         "siteFooterSmall": "&copy; 2005‑now Clearleft Ltd. All rights reserved.",
         "socialLinks": "@social-media.socialLinks",
         "metadata": [{
