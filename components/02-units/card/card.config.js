78dac9c29414e562588ef7d0a02946c0ef70dc8b
Graham Smith <g@blaxian.com>
2016-12-12 16:45:57 +0000

    updates the card pattern to the latest design. Some hacky CSS in there. Will clean up later... promise

--- components/02-units/card/card.config.js
+++ components/02-units/card/card.config.js
@@ -4,28 +4,15 @@ module.exports = {
     "context": {
         "imageSrc": "@index-card.imageSrc",
         "cardTitle": "Article Title Goes here",
-        "openContext": "Category",
-        "cardDescription": "In my previous article, I wrote about some of the misconceptions with “agile” that I see in organisations. This article will focus on one of them specifically – the common (mis)understanding that agile—and more recently lean start-up methods—are seen as operational [...]"
+        "cardCategory": "Opinion",
+        "cardDescription": "In my previous article, I wrote about some of the misconceptions with “agile” that I see in organisations."
     },
     "variants": [{
-        "name": "Card header modifier",
+        "name": "Tiny Lesson",
         "context": {
-            "cardModifier": "",
-            "openContext": "Tiny Lesson",
+            "cardModifier": "--tiny-lesson",
             "cardTitle": "Article Title Goes here",
-            "indexCard": true,
-            "secondContext": "Visual design, 2 hours",
-            "cardDescription": false
-        }
-    },
-    {
-        "name": "A card with an index card and no description",
-        "context": {
-            "openContext": "Tiny Lesson",
-            "cardTitle": "The 20 second gut test",
-            "indexCard": true,
-            "secondContext": "Visual design, 2 hours",
-            "cardDescription": false
+            "cardCategory": "Tiny Lesson"
         }
     }]
 }
