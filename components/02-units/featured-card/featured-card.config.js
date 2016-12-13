4359cfafba29645e88b8bff152f26d1ed10ac1a2
Graham Smith <g@blaxian.com>
2016-12-13 12:19:44 +0000

    add the featured card pattern

---
+++ components/02-units/featured-card/featured-card.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    "status": "wip",
+    "context": {
+        "featuredCardTitle": "Misconceptions about “Agile”",
+        "featuredCardCategory": "Process",
+        "indexCard": true,
+        "imageSrc": "/assets/img/team/james-g.jpg",
+        "indexCardName": "Tea Ivanova",
+        "indexCardInfo": "Project Manager"
+    }
+}
