d9ce5f656368836b88cd3a28cf891ef42af24f84
lottejackson <lottejackson@yahoo.co.uk>
2016-12-13 12:50:05 +0000

    Move imageBlock into a component

---
+++ components/02-units/image-block/image-block.config.js
@@ -0,0 +1,8 @@
+'use strict';
+module.exports = {
+    "context": {
+        "imageBlockSrc": "/assets/img/work/ch4/ch4-team.jpg",
+        "imageBlockAlt": "Channel 4 Team",
+        "imageBlockCaption": "The channel 4 news team. Image courtesy of ITN News"
+    }
+}
