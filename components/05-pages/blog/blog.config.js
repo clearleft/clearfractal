7aeeced5d24f56bbe5a177834a299ad6ed5749dd
Graham Smith <g@blaxian.com>
2016-12-13 16:36:20 +0000

    updates the blog page prototype with patterns

---
+++ components/05-pages/blog/blog.config.js
@@ -0,0 +1,31 @@
+
+const faker = require('faker');
+
+let posts = [];
+
+for (var i = 0; i < 12; i++) {
+    posts.push({
+        cardTitle: "Article name here",
+        cardCategory: 'Opinion',
+        cardDescription: faker.lorem.paragraph()
+    });
+}
+
+module.exports = {
+    "title": "Blog",
+    "status": "wip",
+    "context": {
+        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+        "mastheadTitle": "Show me every post",
+
+        "posts": posts,
+
+        "indexCard": true,
+        "imageSrc": "/assets/img/team/james-g.jpg",
+        "indexCardName": "Tea Ivanova",
+        "indexCardInfo": "Project Manager",
+
+        "inputLabel": "@input-group.inputLabel",
+        "siteFooterSmall": "@site-footer.siteFooterSmall"
+    }
+}
