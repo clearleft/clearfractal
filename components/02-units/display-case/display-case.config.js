efac3c71b219f6c289454e5e4f3e16753a51247a
lottejackson <lottejackson@yahoo.co.uk>
2016-12-14 17:02:35 +0000

    Start display case

---
+++ components/02-units/display-case/display-case.config.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = {
+    "status": "wip",
+    "context": {
+    },
+    "variants": [{
+        "name": "Channel 4",
+        "context": {
+            "displayCaseModifier": "--ch4"
+        }
+    }]
+}
