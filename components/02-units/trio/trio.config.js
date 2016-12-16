60388c512def4ba0ceecaa1da8593f80192964c7
lottejackson <lottejackson@yahoo.co.uk>
2016-12-16 17:40:07 +0000

    Finish trio and add modifiers

--- components/02-units/trio/trio.config.js
+++ components/02-units/trio/trio.config.js
@@ -4,7 +4,7 @@ module.exports = {
         "trioHeading": "The Results",
         "trioItems": [
             { "heading": "Work on products & services", "imageSrc": "/assets/img/icons/rocket.svg", "text": "Radically improve existing products and services, or create them from scratch. Optimise a key interaction to get big results, give your website a new lease of life, or work with the team to conceive, create and build on an entirely new idea." },
-            { "heading": "Get clear on your strategy", "imageSrc": "/assets/img/icons/rocket.svg", "text": "You might need a targeted intervention, to unpick a thorny problem, or a fresh perspective on your process. Our strategists work with you to research and develop an appropriate digital strategy that integrates with your organisation." },
+            { "trioItemModifier": "--rebel", "heading": "Get clear on your strategy", "imageSrc": "/assets/img/icons/rocket.svg", "text": "You might need a targeted intervention, to unpick a thorny problem, or a fresh perspective on your process. Our strategists work with you to research and develop an appropriate digital strategy that integrates with your organisation." },
             { "heading": "Transform your digital culture", "imageSrc": "/assets/img/icons/rocket.svg", "text": "Upskill your team, develop your processes, integrate design thinking and introduce new habits. By working closely with the Clearleft team as part of your in-house process, or in a bespoke workshop setting, you get an external perspective that’s directly relevant to your needs." }
         ]
     },
