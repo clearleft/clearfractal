114634daef6ffd1ea21ae78f355bba1c3616334f
lottejackson <lottejackson@yahoo.co.uk>
2016-12-30 18:29:39 +0000

    Start of the home paeg

---
+++ components/05-pages/home/home.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    "status": "wip",
+    "context": {
+        "trioItems": "@trio.trioItems",
+        "helloHeading": "@hello.helloHeading",
+        "helloBody": "@hello.helloBody",
+        "inputLabel": "Feed your curiosity.",
+        "siteFooterSmall": "&copy; 2005‑now Clearleft Ltd. All rights reserved.",
+        "socialLinks": "@social-media.socialLinks"
+    }
+}
