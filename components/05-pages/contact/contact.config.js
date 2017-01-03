9c5b6086dc2f7b638ff7fd28cb168c52e1aeebdd
lottejackson <lottejackson@yahoo.co.uk>
2017-01-03 17:45:27 +0000

    A start to the contact page components

---
+++ components/05-pages/contact/contact.config.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = {
+    "status": "wip",
+    "context": {
+        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+        "primaryNavModifier": "@primary-navigation.primaryNavModifier",
+        "leadText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore sapiente molestiae sed? Consectetur illo repellat dolorum assumenda, voluptatum explicabo architecto?",
+        "leadModifier": "--wrapper Bar"
+    }
+}
