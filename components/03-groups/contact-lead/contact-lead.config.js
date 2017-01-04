78f5057bfde3215d20d044efa5b96daca8b06097
lottejackson <lottejackson@yahoo.co.uk>
2017-01-04 11:54:55 +0000

    Contact card and contact lead component

---
+++ components/03-groups/contact-lead/contact-lead.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "status": "wip",
+    "context": {
+        "leadText": "There’s nothing like meeting face-to-face. Travelling from London Victoria to Brighton is a 60 minute train journey. London Gatwick airport is a 30 minute journey. When you get here, the pleasant walk from the train station is 15 mins in the direction of the sea. We’re right on the beach.",
+        "leadModifier": "--wrapper Bar"
+    }
+}
