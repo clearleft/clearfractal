353d4587b89ccedfa8b2e54ece1272925e13bb1f
lottejackson <lottejackson@yahoo.co.uk>
2017-01-05 14:36:08 +0000

    Create an event card|

---
+++ components/02-units/event-card/event-card.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "status": "wip",
+    "context": {
+        "eventCardContext": "May 24-26 2017  laban, london",
+        "eventCardSrc": "/assets/img/events/uxlondon-logo.png",
+        "eventCardAlt": "UX London",
+        "eventCardUrl": "",
+        "eventCardLink": "Visit UX London",
+        "eventCardDescription": "<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>",
+        "eventCardListTitle": "Visit past versions:",
+        "pastVersions": [
+            { "item": "2016", "itemUrl": "#" },
+            { "item": "2015", "itemUrl": "#" },
+            { "item": "2014", "itemUrl": "#" },
+            { "item": "2013", "itemUrl": "#" }
+        ]
+    }
+}
