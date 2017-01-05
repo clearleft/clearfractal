e6c6f51236298c62d0cf7a0d9322e12e15180386
lottejackson <lottejackson@yahoo.co.uk>
2017-01-05 11:20:23 +0000

    Add chapter component to events

---
+++ components/05-pages/events/events.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "status": "wip",
+    "context": {
+        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+        "primaryNavModifier": "@primary-navigation--inverted.primaryNavModifier",
+        "propositionLogos": "@proposition.propositionLogos",
+        "signpostTitle": "@proposition.signpostTitle",
+        "mastheadTitle": "An interesting heading about events",
+        "leadText": "@lead.leadText",
+        "pageLeadSecondary": true,
+        "siteFooterSmall": "&copy; 2005‑now Clearleft Ltd. All rights reserved.",
+        "inputLabel": "@input-group.inputLabel",
+        "inputGroupSubmit": "Get the Clearletter",
+        "bannerBody": "@banner--notice.bannerBody"
+    }
+};
