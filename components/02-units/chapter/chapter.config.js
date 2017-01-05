9e26d253d55a0cc679a94b413bbcfdb20179a9e7
lottejackson <lottejackson@yahoo.co.uk>
2017-01-05 14:35:50 +0000

    Chapter refactor

--- components/02-units/chapter/chapter.config.js
+++ components/02-units/chapter/chapter.config.js
@@ -1,15 +1,20 @@
 'use strict';
 module.exports = {
     "status": "wip",
-    "collated": true,
     "context": {
         "chapters": [
         { 
             "title": "Feed your curiosity",
-            "paraOne": "We’re a naturally inquisitive group of tinkerers, geeks and explorers. We don’t sit still, instead we actively seek out new skills, challenges and opportunities.",
-            "paraTwo": "We get inspiration from everywhere and learn by experimenting, setting up initiatives and trying things out.",
-            "imageSVG": "/assets/img/chapter/curiosity.svg",
-            "imagePNG": "/assets/img/chapter/curiosity.png"
+            "primaryContent": `
+            <h2>Feed your curiosity</h2>
+            <p>Every individual who works with us has the opportunity to learn, to develop their voice and cultivate their ideas. </p>
+            <p>Our industry was built by people spreading what they’ve learned. We embrace that same spirit of generosity, which is why we’re
+an enthusiastic body of mentors, writers, bloggers, public speakers and events organisers.</p>
+            ` 
+            ,
+            "src": "/assets/img/chapter/curiosity.png",
+            "srcSVG": "/assets/img/chapter/curiosity.svg",
+            "srcAlt": "Feed your curiosity"
         }]
     }
-}
+};
--- components/03-groups/chapters/chapters.config.js
+++ components/03-groups/chapters/chapters.config.js
@@ -5,32 +5,41 @@ module.exports = {
     "context": {
         "chapters": [
         { 
-            "title": "Feed your curiosity.",
-            "paraOne": "We’re a naturally inquisitive group of tinkerers, geeks and explorers. We don’t sit still, instead we actively seek out new skills, challenges and opportunities.",
-            "paraTwo": "We get inspiration from everywhere and learn by experimenting, setting up initiatives and trying things out.",
-            "imageSVG": "/assets/img/chapter/curiosity.svg",
-            "imagePNG": "/assets/img/chapter/curiosity.png"
+            "title": "Feed your curiosity",
+            "primaryContent": `
+            <h2>Feed your curiosity</h2>
+            <p>Every individual who works with us has the opportunity to learn, to develop their voice and cultivate their ideas. </p>
+            <p>Our industry was built by people spreading what they’ve learned. We embrace that same spirit of generosity, which is why we’re
+an enthusiastic body of mentors, writers, bloggers, public speakers and events organisers.</p>
+            ` 
+            ,
+            "src": "/assets/img/chapter/curiosity.png",
+            "srcSVG": "/assets/img/chapter/curiosity.svg",
+            "srcAlt": "Feed your curiosity"
         },
         { 
             "title": "Learn, share. Learn, share",
-            "paraOne": "Every individual who works with us has the opportunity to learn, to develop their voice and cultivate their ideas.",
-            "paraTwo": "Our industry was built by people spreading what they’ve learned. We embrace that same spirit of generosity, which is why we’re an enthusiastic body of mentors, writers, bloggers, public speakers and events organisers.",
-            "imageSVG": "/assets/img/chapter/learn-share.svg",
-            "imagePNG": "/assets/img/chapter/learn-share.png"
+            "primaryContent": `
+            <h2>Learn, share. Learn, share.</h2>
+            <p>Every individual who works with us has the opportunity to learn, to develop their voice and cultivate their ideas.</p>
+            <p>Our industry was built by people spreading what they’ve learned. We embrace that same spirit of generosity, which is why we’re an enthusiastic body of mentors, writers, bloggers, public speakers and events organisers.</p>
+            ` 
+            ,
+            "src": "/assets/img/chapter/learn-share.png",
+            "srcSVG": "/assets/img/chapter/learn-share.svg",
+            "srcAlt": "Learn and share"
         },
         { 
             "title": "Be fleet of foot.",
-            "paraOne": "Sticking to the path can limit where you end up. Being nimble and adaptable allows us to get over hurdles and thrive in unusual circumstances.",
-            "paraTwo": "We prioritise action over deliberation, and conversations over specifications, so that we can stay on our toes and be ready to adapt.",
-            "imageSVG": "/assets/img/chapter/fleet-of-foot.svg",
-            "imagePNG": "/assets/img/chapter/fleet-of-foot.png"
-        },
-        { 
-            "title": "Be great over good.",
-            "paraOne": "Sticking to the path can limit where you end up. Being nimble and adaptable allows us to get over hurdles and thrive in unusual circumstances.",
-            "paraTwo": "We prioritise action over deliberation, and conversations over specifications, so that we can stay on our toes and be ready to adapt.",
-            "imageSVG": "/assets/img/chapter/great-over-good.svg",
-            "imagePNG": "/assets/img/chapter/great-over-good.png"
+            "primaryContent": `
+            <h2>Be fleet of foot.</h2>
+            <p>Sticking to the path can limit where you end up. Being nimble and adaptable allows us to get over hurdles and thrive in unusual circumstances.</p>
+            <p>We prioritise action over deliberation, and conversations over specifications, so that we can stay on our toes and be ready to adapt.</p>
+            ` 
+            ,
+            "src": "/assets/img/chapter/fleet-of-foot.png",
+            "srcSVG": "/assets/img/chapter/fleet-of-foot.svg",
+            "srcAlt": "Learn and share"
         }]
     }
 }
