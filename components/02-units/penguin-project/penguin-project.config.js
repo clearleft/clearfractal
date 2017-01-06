3037b046601b59a74bd236dbb75e3a0909c76b5c
lottejackson <lottejackson@yahoo.co.uk>
2017-01-06 19:39:03 +0000

    Refactor showcase into a penguin project. Part one

---
+++ components/02-units/penguin-project/penguin-project.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "status": "wip",
+    "context": {
+        "penguinContext": "Penguin random house publishing company",
+        "penguinTitle": "Create a new universe.",
+        "penguinLink": "How do you bring more than 700 brands together into one website?",
+        "penguinLinkUrl": "#"
+    },
+    "variants": [{
+        "name": "Stage",
+        "context": {
+            "penguinModifier": "--stage",
+            "penguinDescription": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dignissimos recusandae eaque cupiditate, ad quia unde porro illo. Minima, ad.",
+            "penguinLogo": "/assets/img/work/penguin/penguin-logo.svg",
+            "penguinLogoAlt": "Penguin logo",
+            "penguinLink": ""
+        }
+    }]
+};
--- components/05-pages/home/home.config.js
+++ components/05-pages/home/home.config.js
@@ -5,6 +5,14 @@ module.exports = {
         "trioItems": "@trio.trioItems",
         "helloHeading": "@hello.helloHeading",
         "helloBody": "@hello.helloBody",
+
+        /*Penguin feature*/
+        "penguinContext": "Penguin random house publishing company",
+        "penguinTitle": "Create a new universe.",
+        "penguinLink": "How do you bring more than 700 brands together into one website?",
+        "penguinLinkUrl": "#",
+
+        /*Footer*/
         "inputLabel": "Feed your curiosity.",
         "inputGroupSubmit": "Get the Clearletter",
         "siteFooterSmall": "&copy; 2005‑now Clearleft Ltd. All rights reserved.",
