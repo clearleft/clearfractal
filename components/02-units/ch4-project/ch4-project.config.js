1fc262e0d17f0cef7755c84451318ce98931d2a8
lottejackson <lottejackson@yahoo.co.uk>
2017-01-06 21:34:39 +0000

    Start on the ch4 project refactor

---
+++ components/02-units/ch4-project/ch4-project.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "status": "wip",
+    "context": {
+        "ch4Context": "ch4 random house publishing company",
+        "ch4Title": "Create a new universe.",
+        "ch4Link": "How do you bring more than 700 brands together into one website?",
+        "ch4LinkUrl": "#",
+        "ch4LogoSrc": "/assets/img/work/ch4/ch4-logo.svg",
+        "ch4LogoAlt": "ch4 logo",
+        "ch4MainSrc": "/assets/img/work/ch4/sling.png"
+    },
+    "variants": [{
+        "name": "Stage",
+        /*The Stage variation is taller */
+        "context": {
+            "ch4Modifier": "--stage",
+            "ch4Description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dignissimos recusandae eaque cupiditate, ad quia unde porro illo. Minima, ad.",
+            "ch4LogoSrc": "/assets/img/work/ch4/ch4-logo.svg",
+            "ch4LogoAlt": "ch4 logo",
+            "ch4Link": ""
+        }
+    }]
+};
--- components/02-units/penguin-project/penguin-project.config.js
+++ components/02-units/penguin-project/penguin-project.config.js
@@ -8,26 +8,13 @@ module.exports = {
     },
     "variants": [{
         "name": "Stage",
-        /*The Stage variation is used on the case study page and masthead */
+        /*The Stage variation is taller */
         "context": {
             "penguinModifier": "--stage",
             "penguinDescription": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dignissimos recusandae eaque cupiditate, ad quia unde porro illo. Minima, ad.",
             "penguinLogo": "/assets/img/work/penguin/penguin-logo.svg",
             "penguinLogoAlt": "Penguin logo",
             "penguinLink": ""
-        }
-    },
-    {
-        "name": "Work",
-        /*The Stage variation is used on the case study page and masthead */
-        "context": {
-            "penguinModifier": "--stage",
-            "penguinDescription": "",
-            "penguinLogo": "",
-            "penguinLogoAlt": "",
-            "penguinLink": "",
-            "penguinLink": "How do you bring more than 700 brands together into one website?",
-            "penguinLinkUrl": "#"
         }
     }]
 };
