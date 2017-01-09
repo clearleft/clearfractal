8ed0e85f347bd7bdc86139efa7d42d704daaad38
lottejackson <lottejackson@yahoo.co.uk>
2017-01-09 18:16:14 +0000

    Refactor penguin

--- components/02-units/penguin-project/penguin-project.config.js
+++ components/02-units/penguin-project/penguin-project.config.js
@@ -20,10 +20,17 @@ module.exports = {
         "name": "Main stage case study",
         "context": {
             "penguinModifier": "--stage",
+            "penguinContext": false,
             "penguinDescription": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dignissimos recusandae eaque cupiditate, ad quia unde porro illo. Minima, ad.",
             "penguinLogo": "/assets/img/work/penguin/penguin-logo.svg",
             "penguinLogoAlt": "Penguin logo",
-            "penguinLink": ""
+            "penguinLink": "",
+            "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+            "primaryNavModifier": "@primary-navigation--inverted.primaryNavModifier",
+            "logoModifier": "--inverted",
+            "segmentOne": "Our Work",
+            "segmentTwo": "Channel 4",
+            "breadcrumbModifier": "--inverted"
         }
     }]
 };
