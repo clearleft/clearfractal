0e5d3a599e41ca6cbc191afcc0c1f070efeac059
lottejackson <lottejackson@yahoo.co.uk>
2017-01-10 14:51:22 +0000

    Refactor pagination

--- components/02-units/pagination/pagination.config.js
+++ components/02-units/pagination/pagination.config.js
@@ -1,3 +1,15 @@
+var items = [];
+for(var i = 1; i < 5; i++){
+    items.push({
+        pageNumber: i
+    });
+}
+
 module.exports = {
-    "status": "wip"
+    "status": "wip",
+    "context": {
+        "items": items,
+        "pagePrevUrl": "#",
+        "pageNextUrl": "#"
+    }
 }
