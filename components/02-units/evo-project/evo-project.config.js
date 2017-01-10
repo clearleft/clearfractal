ed50493001a669538d676b07c7eb8fc99f0c2f15
lottejackson <lottejackson@yahoo.co.uk>
2017-01-10 08:36:31 +0000

    start Evo styles

---
+++ components/02-units/evo-project/evo-project.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "status": "wip",
+    "context": {
+        "evoContext": "Evo random house publishing company",
+        "evoTitle": "Create a new universe.",
+        "evoLink": "How do you bring more than 700 brands together into one website?",
+        "evoLinkUrl": "#",
+        "evoLogoSrc": false,
+        "evoLogoAlt": false,
+        "evoMainSrc": "/assets/img/work/evo/sling.png"
+    },
+    "variants": [{
+        "name": "Work page listing",
+        "context": {
+            "evoModifier": "--tall"
+        }
+    },
+    {
+        "name": "Main stage case study",
+        "context": {
+            "evoModifier": "--stage",
+            "evoDescription": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dignissimos recusandae eaque cupiditate, ad quia unde porro illo. Minima, ad.",
+            "evoLogoSrc": "/assets/img/work/evo/evo-logo.svg",
+            "evoLogoAlt": "evo logo",
+            "evoLink": "",
+            "evoContext": false,
+            "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+            "primaryNavModifier": "@primary-navigation--inverted.primaryNavModifier",
+            "logoModifier": "--inverted",
+            "segmentOne": "Our Work",
+            "segmentTwo": "Channel 4",
+            "breadcrumbModifier": "--inverted"
+        }
+    }]
+};
