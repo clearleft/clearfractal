352fb149f00d43b27b32558a51ae895e171a9cb8
lottejackson <lottejackson@yahoo.co.uk>
2017-01-12 18:22:56 +0000

    Penguin page refactor

--- components/05-pages/penguin-featured/penguin-featured.config.js
+++ components/05-pages/penguin-featured/penguin-featured.config.js
@@ -2,13 +2,6 @@
 module.exports = {
     "status": "wip",
     "context": {
-        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
-        "primaryNavModifier": "@primary-navigation--inverted.primaryNavModifier",
-        "logoModifier": "--inverted",
-        "segmentOne": "Our Work",
-        "segmentTwo": "Channel 4",
-        "breadcrumbModifier": "--inverted",
-
         /*Penguin Project feature*/
         "penguinTitle": "Create a new universe.",
         "penguinModifier": "--stage",
@@ -17,6 +10,8 @@ module.exports = {
         "penguinLogo": "/assets/img/work/penguin/penguin-logo.svg",
         "penguinLogoAlt": "Penguin logo",
         "penguinLink": "",
+
+        /*Header*/
         "primaryNavlinks": "@primary-navigation.primaryNavlinks",
         "primaryNavModifier": "@primary-navigation--inverted.primaryNavModifier",
         "logoModifier": "--inverted",
@@ -24,9 +19,35 @@ module.exports = {
         "segmentTwo": "Channel 4",
         "breadcrumbModifier": "--inverted",
 
+        /*Globals*/
         "signpostItems": "@signpost.signpostItems",
         "helloHeading": "@hello.helloHeading",
         "helloBody": "@hello.helloBody",
+
+        /*Voice*/
+        "voiceDescription": "We’re in the news room, in the centre of all the activity, watching the team capture, edit, and share events from all over the world.  It’s fast-moving, exciting, and iterative. In that moment, I realised that we could also design in a way that harnesses that same excitement.",
+        "voiceCite": "James Box, UX Director, Team lead",
+        "voicePersonSrc": "/assets/img/team/voice/graham.png",
+        "voicePersonAlt": "Graham Smith",
+
+        /*Scene*/
+        "metas": "@scene.metas",
+        "sceneModifier": "--shuffle Scene--featured",
+
+        /*Content*/
+        "imageBlockSrc": "/assets/img/work/penguin/display-case/co-design-session.jpg",
+        "imageBlockAlt": "@image-block.imageBlockAlt",
+        "imageBlockCaption": "conducting co-design sessions at prh, vauxhaul bridge",
+        "textBlockProse": `
+        <h2>How do you create a single strategy for stakeholders who may also be competitors?</h2>
+        <p>Penguin Random House believed that the new Penguin website could help people discover the book they wanted to read next. At Clearleft, we love a good provocation to get theories and opinions out on the table; during one of our early workshops we asked a question of the stakeholders:</p>
+        `,
+
+        /*Quote*/
+        "quoteAvatarSrc": "/assets/img/work/ch4/anna-doble.jpg",
+        "quoteAvatarAlt": "@quote.quoteAvatarAlt",
+        "quoteText": "@quote.quoteText",
+        "quoteAuthor": "@quote.quoteAuthor",
         
         /*Footer*/
         "footerLinks": "@site-footer.footerLinks",
