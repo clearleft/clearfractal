d553728d927d324f04f7a525da7283e4cd699aaa
Jeremy Keith <jeremy@adactio.com>
2017-01-14 17:39:58 +0000

    Behaviour for showing/hiding content

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/*
+Show and hide content with aria-controls buttons
+*/
+(function (win, doc) {
+    'use strict';
+    if (!win.addEventListener) {
+        // doesn't cut the mustard.
+        return;
+    }
+    var toggleID;
+    var togglecontent;
+    var target;
+    function toggle(ev) {
+        ev = ev || win.event;
+        target = ev.target;
+        if (target.hasAttribute('aria-controls')) {
+            toggleID = target.getAttribute('aria-controls');
+            if (doc.getElementById(toggleID)) {
+                ev.preventDefault();
+                togglecontent = doc.getElementById(toggleID);
+                if (togglecontent.getAttribute('aria-hidden') == 'true') {
+                    togglecontent.setAttribute('aria-hidden', 'false');
+                    target.setAttribute('aria-expanded', 'true');
+                } else {
+                    togglecontent.setAttribute('aria-hidden', 'true');
+                    target.setAttribute('aria-expanded', 'false');
+                }
+            }
+        }
+    }
+    doc.addEventListener('click', toggle, false);
+}(this, this.document));
