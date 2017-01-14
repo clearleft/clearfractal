693575aa7237fd2ae90eef62d37b5e9b920dd4c6
Jeremy Keith <jeremy@adactio.com>
2017-01-14 17:40:30 +0000

    Content for blog post selector

--- components/02-units/selector/selector.config.js
+++ components/02-units/selector/selector.config.js
@@ -1,5 +1,43 @@
+
 module.exports = {
     "status": "wip",
     "context": {
-    }
+        "selectorID": "fragmentidentifier",
+        "triggerContent": "trigger label",
+        "targetContent": "<p>Target content.</p>"
+    },
+    "variants": [{
+        "name": "posts",
+        "context": {
+            "selectorID": "typesofpost",
+            "triggerContent": "all posts",
+            "triggerClass": "Heading Heading--main",
+            "targetClass": "Container BgGreen inverted",
+            "listContext": {
+                "listClass": "",
+                "listItemClass": "Heading Heading--secondary",
+                "linkClass": "",
+                "listItems": [{
+                    "url": "#",
+                    "content": "opinions"
+                },
+                {
+                    "url": "#",
+                    "content": "announcements"
+                },
+                {
+                    "url": "#",
+                    "content": "podcasts"
+                },
+                {
+                    "url": "#",
+                    "content": "tiny lessons"
+                },
+                {
+                    "url": "#",
+                    "content": "all posts"
+                }]
+            }
+        }
+    }]
 }
