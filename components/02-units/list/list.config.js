fb8e5d6569fe12ba3e44df626f3c1d8ddc739d09
lottejackson <lottejackson@yahoo.co.uk>
2017-01-16 16:21:38 +0000

    Add tall variant to list

--- components/02-units/list/list.config.js
+++ components/02-units/list/list.config.js
@@ -3,5 +3,11 @@ module.exports = {
     "status": "wip",
     "context": {
         "listItem": "A single item in a list"
-    }
+    },
+    "variants": [{
+        "name": "Tall",
+        "context": {
+            "listModifier": "--tall"
+        }
+    }]
 }
