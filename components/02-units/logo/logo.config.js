1aef3ba835c158938292877f154a082f5c90948b
lottejackson <lottejackson@yahoo.co.uk>
2017-01-16 15:59:37 +0000

    Add variants for different colour logo streaks

--- components/02-units/logo/logo.config.js
+++ components/02-units/logo/logo.config.js
@@ -4,9 +4,39 @@ module.exports = {
         "logoURL": "#"
     },
     "variants": [{
-        "name": "Inverted",
+        "name": "Inverted text",
         "context": {
             "logoModifier": "--inverted"
+        }
+    },
+    {
+        "name": "Purple streak",
+        "context": {
+            "logoModifier": "--purple"
+        }
+    },
+    {
+        "name": "Red streak",
+        "context": {
+            "logoModifier": "--red"
+        }
+    },
+    {
+        "name": "Blue streak",
+        "context": {
+            "logoModifier": "--blue"
+        }
+    },
+    {
+        "name": "Yellow streak",
+        "context": {
+            "logoModifier": "--yellow"
+        }
+    },
+    {
+        "name": "Light blue streak",
+        "context": {
+            "logoModifier": "--lightBlue"
         }
     }]
 };
