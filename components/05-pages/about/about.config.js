f7f338a354fd0dced5de2e65389adc0cfa9e9bbd
lottejackson <lottejackson@yahoo.co.uk>
2017-01-17 17:06:58 +0000

    Finish both image galleries

--- components/05-pages/about/about.config.js
+++ components/05-pages/about/about.config.js
@@ -2,7 +2,7 @@
 const faker     = require('faker');
 const persons   = [];
 var imageSrc = ["/assets/img/team/james-g.jpg", "/assets/img/team/ben-w.jpg", "/assets/img/team/ellen.jpg", "/assets/img/team/hana.jpg", "/assets/img/team/charlotte.jpg", "/assets/img/team/andy-t.jpg"];
-for(var i = 0; i < 26; i++){
+for(var i = 0; i < 25; i++){
     var number = Math.floor((Math.random() * 5) + 1);
     persons.push({
         indexCardName: faker.name.findName(),
