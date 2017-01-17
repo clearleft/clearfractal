82224159841df25d7c167268c2a344f8cfc29262
lottejackson <lottejackson@yahoo.co.uk>
2017-01-17 14:29:54 +0000

    Fix var name in config issue #82

--- components/03-groups/blog-extension/blog-extension.config.js
+++ components/03-groups/blog-extension/blog-extension.config.js
@@ -5,13 +5,13 @@ module.exports = {
     "context": {
         cards: [
         {
-            "openContext": "@card.openContext",
+            "cardCategory": "@card.cardCategory",
             "cardTitle": "@card.cardTitle",
             "secondContext": "Visual design, 2 hours",
             "cardDescription": "In my previous article, I wrote about some of the misconceptions with “agile” that I see in organisations. This article will focus on one of them specifically – the common (mis)understanding that agile—and more recently lean start-up methods—are seen as operational [...]"
         },
         {
-            "openContext": "@card.openContext",
+            "cardCategory": "@card.cardCategory",
             "cardTitle": "@card.cardTitle",
             "secondContext": "Visual design, 2 hours",
             "cardDescription": "In my previous article, I wrote about some of the misconceptions with “agile” that I see in organisations. This article will focus on one of them specifically – the common (mis)understanding that agile—and more recently lean start-up methods—are seen as operational [...]"
