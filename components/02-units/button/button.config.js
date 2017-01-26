d6422a92d57277180c6e60bfe8dd8c4ce1acc508
Jeremy Keith <jeremy@adactio.com>
2017-01-26 15:25:17 +0000

    Revert "Remove button"
    This reverts commit 02538b5bec0220c8d7d298beeb1d6c55d4bf795b.

---
+++ components/02-units/button/button.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "status": "wip",
+    "collated": true,
+    "preview": "@preview-dark",
+    "context": {
+        "buttonText": "Check out our open positions &#10141;"
+    }
+}
