02538b5bec0220c8d7d298beeb1d6c55d4bf795b
Jeremy Keith <jeremy@adactio.com>
2017-01-26 15:11:04 +0000

    Remove button

--- components/02-units/button/button.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "status": "wip",
-    "collated": true,
-    "preview": "@preview-dark",
-    "context": {
-        "buttonText": "Check out our open positions &#10141;"
-    }
-}
