78c843c314792b0ff6e00bc732226ad8599c4806
Graham Smith <g@blaxian.com>
2017-02-08 16:53:38 +0000

    adds new TextBlock with image variant

--- components/02-units/text-block/text-block.config.js
+++ components/02-units/text-block/text-block.config.js
@@ -9,23 +9,32 @@ module.exports = {
             <p>That’s largely due to the various interpretations of the manifesto borne of misunderstanding. Bottom-up as well as top-down support is needed in order for a successful agile transformation to take place in any organisation.</p>
         `
     },
-    "variants": [{
-        "name": "Author",
-        "context": {
-            "textBlockModifier": "--author",
-            "authorImage": "@author.authorImage",
-            "authorName": "@author.authorName"
+    "variants": [
+        {
+            "name": "Author",
+            "context": {
+                "textBlockModifier": "--author",
+                "authorImage": "@author.authorImage",
+                "authorName": "@author.authorName"
+            }
+        },
+        {
+            "name": "Author and Lead",
+            "context": {
+                "textBlockModifier": "--author Bar Lead",
+                "authorImage": "@author.authorImage",
+                "authorName": "@author.authorName",
+                "textBlockProse": `<p>The agile manifesto was born out of a frustration with traditionally managed projects. But I don’t think it solved the problem. It actually created another one. I’ve seen teams and organisations go from waterfall, which reduces their ability to compete in the market and deliver value, to dysfunction and paralysis when implementing agile.</p>`
+            }
+        },
+        {
+            "name": "With image",
+            "context": {
+                "textBlockModifier": "--image",
+                "textBlockImageDirection": "right",
+                "textBlockImage": "http://placekitten.com/420/420",
+                "textBlockProse": `<p>The agile manifesto was born out of a frustration with traditionally managed projects. But I don’t think it solved the problem. It actually created another one. I’ve seen teams and organisations go from waterfall, which reduces their ability to compete in the market and deliver value, to dysfunction and paralysis when implementing agile.</p>`
+            }
         }
-    },
-    {
-        "name": "Author and Lead",
-        "context": {
-            "textBlockModifier": "--author Bar Lead",
-            "authorImage": "@author.authorImage",
-            "authorName": "@author.authorName",
-            "textBlockProse": `
-                <p>The agile manifesto was born out of a frustration with traditionally managed projects. But I don’t think it solved the problem. It actually created another one. I’ve seen teams and organisations go from waterfall, which reduces their ability to compete in the market and deliver value, to dysfunction and paralysis when implementing agile.</p>
-            `
-        }
-    }]
+    ]
 }
