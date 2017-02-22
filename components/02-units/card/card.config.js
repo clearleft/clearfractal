01c0c9067d1d41e860ceda0006cbab06b0797f0a
Graham Smith <g@blaxian.com>
2017-02-22 16:52:19 +0000

    many tweaks. much wow

--- components/02-units/card/card.config.js
+++ components/02-units/card/card.config.js
@@ -1,4 +1,5 @@
 'use strict';
+
 module.exports = {
     "status": "ready",
     "context": {
@@ -9,31 +10,33 @@ module.exports = {
         "cardDescription": "In my previous article, I wrote about some of the misconceptions with “agile” that I see in organisations.",
         "cardLink": "#"
     },
-    "variants": [{
-        "name": "Tiny Lesson",
-        "context": {
-            "cardModifier": "--tiny-lesson",
-            "cardTitle": "Article Title Goes here",
-            "cardCategory": "Tiny Lesson"
+    "variants": [
+        {
+            "name": "Tiny Lesson",
+            "context": {
+                "cardModifiers": ["--tiny-lesson"],
+                "cardTitle": "Article Title Goes here",
+                "cardCategory": "Tiny Lesson"
+            }
+        },
+        {
+            "name": "Vibrant",
+            "context": {
+                "cardModifiers": ["--vibrant"],
+                "cardTitle": "Royal Borough of Kensington and Chelsea",
+                "cardCategory": false,
+                "cardDescription": false,
+                "cardLink": "#"
+            }
+        },
+        {
+            "name": "With author",
+            "context": {
+                "cardModifiers": ["--image"],
+                "cardTitle": "Royal Borough of Kensington and Chelsea",
+                "authorImage": "/assets/img/authors/sm/edv.png",
+                "authorName": "Ellen DeVries"
+            }
         }
-    },
-    {
-        "name": "Vibrant",
-        "context": {
-            "cardModifier": "--vibrant",
-            "cardTitle": "Royal Borough of Kensington and Chelsea",
-            "cardCategory": false,
-            "cardDescription": false,
-            "cardLink": "#"
-        }
-    },
-    {
-        "name": "With author",
-        "context": {
-            "cardModifier": "--image",
-            "cardTitle": "Royal Borough of Kensington and Chelsea",
-            "authorImage": "/assets/img/authors/sm/edv.png",
-            "authorName": "Ellen DeVries"
-        }
-    }]
+    ]
 }
--- components/02-units/ch4-project/ch4-project.config.js
+++ components/02-units/ch4-project/ch4-project.config.js
@@ -1,35 +1,43 @@
 module.exports = {
     "status": "wip",
     "context": {
-        "ch4Context": "ch4 random house publishing company",
-        "ch4Title": "Create a new universe.",
-        "ch4Link": "How do you bring more than 700 brands together into one website?",
+        "ch4Question": "How do you create a new kind of news service?",
+        "ch4Title": "Focus on the intelligent story.",
         "ch4LinkUrl": "#",
-        "ch4LogoSrc": false,
-        "ch4LogoAlt": false,
-        "ch4MainSrc": "/assets/img/work/ch4/c4-mag-glass.png"
+        "ch4LogoSrc": "/assets/img/work/ch4/ch4-logo.svg",
+        "ch4LogoAlt": "ch4 logo",
     },
-    "variants": [{
-        "name": "Work page listing",
-        "context": {
-            "ch4Modifier": "--tall"
+    "variants": [
+        {
+            "name": "Home page listing",
+            "context": {
+                "ch4Modifier": "--short"
+            }
+        },
+        {
+            "name": "Work page listing",
+            "context": {
+                "ch4Modifier": "--tall"
+            }
+        },
+        {
+            "name": "Main stage case study",
+            "context": {
+                "ch4Modifier": "--stage",
+
+                "logoModifiers": ["--inverted"],
+                "primaryNavModifiers": ["--inverted"],
+                "primaryNavlinks": "@primary-navigation.primaryNavlinks",
+
+                "segmentOne": "Our Work",
+                "segmentTwo": "Channel 4",
+                "breadcrumbModifier": "--inverted",
+
+                "ch4Description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dignissimos recusandae eaque cupiditate, ad quia unde porro illo. Minima, ad.",
+                "ch4LogoSrc": "/assets/img/work/ch4/ch4-logo.svg",
+                "ch4LogoAlt": "ch4 logo",
+                "ch4Link": "",
+            }
         }
-    },
-    {
-        "name": "Main stage case study",
-        "context": {
-            "ch4Modifier": "--stage",
-            "ch4Description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dignissimos recusandae eaque cupiditate, ad quia unde porro illo. Minima, ad.",
-            "ch4LogoSrc": "/assets/img/work/ch4/ch4-logo.svg",
-            "ch4LogoAlt": "ch4 logo",
-            "ch4Link": "",
-            "ch4Context": false,
-            "primaryNavlinks": "@primary-navigation.primaryNavlinks",
-            "primaryNavModifier": "@primary-navigation--inverted.primaryNavModifier",
-            "logoModifier": "--inverted",
-            "segmentOne": "Our Work",
-            "segmentTwo": "Channel 4",
-            "breadcrumbModifier": "--inverted"
-        }
-    }]
+    ]
 };
