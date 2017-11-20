d0cff17fc649052e59cacf81f248d2324df91069
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-20 18:04:12 +0000

    Begin building Tabs

---
+++ components/02-units/tabs/panellist/panellist.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    status: 'wip',
+    collated: true,
+    context: {
+        tabs: [{
+            panelContent: `<p>Panel 1</p>`
+        },
+        {
+            panelContent: `<p>Panel 2</p>`
+        },
+        {
+            panelContent: `<p>Panel 3</p>`
+        }
+        ]
+    }
+}
---
+++ components/02-units/tabs/tablist/tablist.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    status: 'wip',
+    collated: true,
+    context: {
+        tabs: [{
+                tabText: 'Section 1',
+                tabId: 'tab-01',
+                tabURL: '#section-01'
+            },
+            {
+                tabText: 'Section 2',
+                tabId: 'tab-02',
+                tabURL: '#section-02'
+            },
+            {
+                tabText: 'Section 3',
+                tabId: 'tab-03',
+                tabURL: '#section-03'
+            }
+        ]
+    }
+}
