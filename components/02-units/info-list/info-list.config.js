de198d99a461bc99a516b86c7a164f47758b892f
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-21 15:41:17 +0000

    Add info-list component

---
+++ components/02-units/info-list/info-list.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    status: 'wip',
+    collated: true,
+    context: {
+        info: [
+            {
+                title: 'Date',
+                type: 'date',
+                content: '25-27 Oct'
+            },
+            {
+                title: 'Location',
+                type: 'location',
+                content: 'The Barbican Centre, London'
+            },
+            {
+                title: 'Location',
+                content: 'The Barbican Centre, London'
+            }
+        ]
+    }
+}
