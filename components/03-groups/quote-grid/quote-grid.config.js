38b37a182686a153a4eca3be608af786e19e742b
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-22 12:13:54 +0000

    Add Quote grid component

---
+++ components/03-groups/quote-grid/quote-grid.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    status: 'wip',
+    context: {
+        quotes: [
+            {
+                quoteAvatarSrc: '/assets/img/work/ch4/anna-doble.jpg',
+                quoteAvatarAlt: 'James Bates',
+                quoteText: 'I remember a really good sense of collaboration. There were a lot of moving parts—us at Clearleft, the team at Channel 4, but also the team at ITN. I think we had a really good working relationship between all of those people.',
+                quoteAuthor: 'James Bates'
+            },
+            {
+                quoteText: 'I remember a really good sense of collaboration. There were a lot of moving parts—us at Clearleft, the team at Channel 4, but also the team at ITN. I think we had a really good working relationship between all of those people.',
+                quoteAuthor: 'James Bates'
+            },
+            {
+                quoteAvatarSrc: '/assets/img/work/ch4/anna-doble.jpg',
+                quoteAvatarAlt: 'Don Norman',
+                quoteText: 'I came. I listened. I stayed. Great event, great speakers, great crowd, great venue. Learn, network. enjoy',
+                quoteAuthor: 'Don Norman',
+                quoteAuthorRole: 'Speaker at UX London 2009'
+            },
+            {
+                quoteText: 'I came. I listened. I stayed. Great event, great speakers, great crowd, great venue. Learn, network. enjoy',
+                quoteAuthor: 'Don Norman',
+                quoteAuthorRole: 'Speaker at UX London 2009'
+
+            }]
+    }
+}
