1549d21648149fd9fe3f1028aabd41763fce7d68
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-22 10:32:38 +0000

    Add Stat component

---
+++ components/03-groups/stats/stats.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    status: 'wip',
+    collated: true,
+    variants: [
+        {
+            name: 'ranges',
+            context: {
+                stats: [{
+                        statValue: '20,000,000+',
+                        statCaption: 'Attend'
+                    },
+                    {
+                        statValue: '65',
+                        statCaption: 'Posts'
+                    },
+                    {
+                        statValue: '20',
+                        statCaption: 'Sharpies Lost on the Field of Battle'
+                    },
+                    {
+                        statValue: '300,000,000,000',
+                        statCaption: 'Sharpies Lost on the Field of Battle'
+                    }
+                ]
+            }
+        }, {
+            name: 'representative',
+            context: {
+                stats: [{
+                        statValue: '20,000+',
+                        statCaption: 'Attendees'
+                    },
+                    {
+                        statValue: '235',
+                        statCaption: 'Speakers'
+                    },
+                    {
+                        statValue: '65',
+                        statCaption: 'Workshops run'
+                    },
+                    {
+                        statValue: '300m',
+                        statCaption: 'Post-its used'
+                    },
+                    {
+                        statValue: '25bn',
+                        statCaption: 'Sharpies lost'
+                    }
+                ]
+            }
+        }
+    ]
+}
