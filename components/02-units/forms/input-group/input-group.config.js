20acaafba0240ac7655acfd074d714df75345299
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-22 13:59:47 +0000

    Add highlight input-group variant

--- components/02-units/forms/input-group/input-group.config.js
+++ components/02-units/forms/input-group/input-group.config.js
@@ -1,11 +1,29 @@
 'use strict';
 module.exports = {
-    "status": "wip",
-    "context": {
-        "inputGroupURL": "#",
-        "inputGroupLabel": "Form input label",
-        "inputGroupSubmit": "Get the Clearletter",
-        "inputGroupID": "ID1",
-        "inputGroupPlaceholder": "E.g. example@domain.com"
-    }
+    status: 'wip',
+    context: {
+        inputGroupURL: '#',
+        inputGroupLabel: 'Form input label',
+        inputGroupSubmit: 'Get the Clearletter',
+        inputGroupID: 'ID1',
+        inputGroupPlaceholder: 'E.g. example@domain.com'
+    },
+    variants: [{
+            name: 'highlight',
+            context: {
+                inputGroupModifiers: 'highlight',
+                inputGroupURL: '#',
+                inputGroupLabel: 'Get notified with updates',
+                inputGroupSubmit: 'Join the mailing list',
+                inputGroupID: 'ID2',
+                inputGroupPlaceholder: 'Enter email address',
+                inputGroupMicrocopy: 'Low volume mailing list. Just for events & conference updates. <a href="#">See an example</a>'
+            }
+        }, {
+            name: 'prominent',
+            context: {
+                inputGroupModifiers: 'prominent'
+            }
+        }
+    ]
 };
