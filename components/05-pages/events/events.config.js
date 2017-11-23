20142589949d899fed92b34e5e79c464e39e7dbf
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-23 12:44:33 +0000

    Apply minor formatting changes; add border util and var

--- components/05-pages/events/events.config.js
+++ components/05-pages/events/events.config.js
@@ -1,123 +1,123 @@
 module.exports = {
-    "status": "wip",
-    "context": {
-        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
-        "primaryNavModifiers": ["--crayola-red"],
-        "logoModifiers": ["--crayola-red"],
+    status: 'wip',
+    context: {
+        primaryNavlinks: '@primary-navigation.primaryNavlinks',
+        primaryNavModifiers: ['--crayola-red'],
+        logoModifiers: ['--crayola-red'],
 
         /*Masthead*/
-        "mastheadTitle": "Events.",
-        "mastheadDescription": "Over the last 12 years we’ve been working hard to advance the practice of design by learning from expert thinkers and makers. In 2005, we created one of the UK’s first web conferences. Since then, we’ve run over 25 conferences.",
-        "mastheadModifier": '--chimney Texture--home',
+        mastheadTitle: 'Our<br>Events.',
+        mastheadDescription: 'Whether a digital leader, designer or developer we have events to inspire you and your team, give you new skills, and bring you the latest digital thinking from globally renowned speakers.',
+        mastheadModifier: '--chimney Texture--home',
 
-        "bannerCaption": "Missions Accomplished",
-        "bannerHeadline": "Our Past Events",
-        "bannerModifier": "--plain",
+        bannerCaption: 'Missions Accomplished',
+        bannerHeadline: 'Our Past Events',
+        bannerModifier: '--plain',
 
-        "currentEvents": [
+        currentEvents: [
             {
-                "src": "http://placehold.it/1330x650",
-                "srcAlt": "Patterns Day",
-                "eventBackground": "url('http://clearleft-v5.s3.amazonaws.com/events/light-tiles.png') 0 50% no-repeat",
-                "eventCardModifier": "",
-                "eventCardContext": "May 24-26 2017  laban, london",
-                "eventCardSrc": "http://placehold.it/380x70",
-                "eventCardAlt": "Patterns Day",
-                "eventCardUrl": "",
-                "eventCardLink": "Visit Event",
-                "eventCardDescription": "<p>Here is some description text. It keeps going a little bit like this and more words are also here.</p>",
-                "eventCardListTitle": "Visit past versions:",
-                "pastVersions": [
-                    { "item": "2016", "itemUrl": "#" },
-                    { "item": "2015", "itemUrl": "#" },
-                    { "item": "2014", "itemUrl": "#" },
-                    { "item": "2013", "itemUrl": "#" }
+                src: 'http://placehold.it/1330x650',
+                srcAlt: 'Patterns Day',
+                eventBackground: `url('http://clearleft-v5.s3.amazonaws.com/events/light-tiles.png') 0 50% no-repeat`,
+                eventCardModifier: '',
+                eventCardContext: 'May 24-26 2017  laban, london',
+                eventCardSrc: 'http://placehold.it/380x70',
+                eventCardAlt: 'Patterns Day',
+                eventCardUrl: '',
+                eventCardLink: 'Visit Event',
+                eventCardDescription: '<p>Here is some description text. It keeps going a little bit like this and more words are also here.</p>',
+                eventCardListTitle: 'Visit past versions:',
+                pastVersions: [
+                    { item: '2016', itemUrl: '#' },
+                    { item: '2015', itemUrl: '#' },
+                    { item: '2014', itemUrl: '#' },
+                    { item: '2013', itemUrl: '#' }
                 ],
             },
             {
-                "src": "http://placehold.it/760x650",
-                "srcAlt": "UXLondon",
-                "eventBackground": "#1F6B9C",
-                "eventCardModifier": "--inverted",
-                "eventCardContext": "May 24-26 2017  laban, london",
-                "eventCardSrc": "http://placehold.it/380x70",
-                "eventCardAlt": "UX London",
-                "eventCardUrl": "",
-                "eventCardLink": "Visit UX London",
-                "eventCardDescription": "<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>",
-                "eventCardListTitle": "Visit past versions:",
-                "pastVersions": [
-                    { "item": "2016", "itemUrl": "#" },
-                    { "item": "2015", "itemUrl": "#" },
-                    { "item": "2014", "itemUrl": "#" },
-                    { "item": "2013", "itemUrl": "#" }
+                src: 'http://placehold.it/760x650',
+                srcAlt: 'UXLondon',
+                eventBackground: '#1F6B9C',
+                eventCardModifier: '--inverted',
+                eventCardContext: 'May 24-26 2017  laban, london',
+                eventCardSrc: 'http://placehold.it/380x70',
+                eventCardAlt: 'UX London',
+                eventCardUrl: '',
+                eventCardLink: 'Visit UX London',
+                eventCardDescription: '<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>',
+                eventCardListTitle: 'Visit past versions:',
+                pastVersions: [
+                    { item: '2016', itemUrl: '#' },
+                    { item: '2015', itemUrl: '#' },
+                    { item: '2014', itemUrl: '#' },
+                    { item: '2013', itemUrl: '#' }
                 ],
             },
             {
-                "src": "http://placehold.it/760x650",
-                "srcAlt": "Leading Design",
-                "eventBackground": "#C5E6F3",
-                "eventCardContext": "October 24-26 2017 london",
-                "eventCardSrc": "https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/leading-design-logo.svg",
-                "eventCardAlt": "UX London",
-                "eventCardUrl": "",
-                "eventCardLink": "Leading Design",
-                "eventCardDescription": "<p>A conference for people leading design teams, overseeing design direction, or instilling a culture of design within their organisations. Listen to leaders from Spotify, 37Signals and Google Ventures discuss the challenges of managing design, and becoming a better design leader.</p>",
-                "eventCardListTitle": "Visit past versions:",
-                "pastVersions": [
-                    { "item": "2016", "itemUrl": "#" }
+                src: 'http://placehold.it/760x650',
+                srcAlt: 'Leading Design',
+                eventBackground: '#C5E6F3',
+                eventCardContext: 'October 24-26 2017 london',
+                eventCardSrc: 'https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/leading-design-logo.svg',
+                eventCardAlt: 'UX London',
+                eventCardUrl: '',
+                eventCardLink: 'Leading Design',
+                eventCardDescription: '<p>A conference for people leading design teams, overseeing design direction, or instilling a culture of design within their organisations. Listen to leaders from Spotify, 37Signals and Google Ventures discuss the challenges of managing design, and becoming a better design leader.</p>',
+                eventCardListTitle: 'Visit past versions:',
+                pastVersions: [
+                    { item: '2016', itemUrl: '#' }
                 ],
             }
         ],
 
-        "pastEvents": [
+        pastEvents: [
             {
-                "src": "http://placehold.it/544x360",
-                "srcAlt": "Leading Design",
-                "eventCardContext": "May 24-26 2017  laban, london",
-                "eventCardSrc": "https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/leading-design-logo.svg",
-                "eventCardAlt": "Leading Design",
-                "eventCardUrl": "",
-                "eventCardLink": "Visit Leading Design",
-                "eventCardDescription": "<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>",
-                "eventCardListTitle": "Visit past versions:",
-                "pastVersions": [
-                    { "item": "2016", "itemUrl": "#" },
-                    { "item": "2015", "itemUrl": "#" },
-                    { "item": "2014", "itemUrl": "#" },
-                    { "item": "2013", "itemUrl": "#" }
+                src: 'http://placehold.it/544x360',
+                srcAlt: 'Leading Design',
+                eventCardContext: 'May 24-26 2017  laban, london',
+                eventCardSrc: 'https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/leading-design-logo.svg',
+                eventCardAlt: 'Leading Design',
+                eventCardUrl: '',
+                eventCardLink: 'Visit Leading Design',
+                eventCardDescription: '<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>',
+                eventCardListTitle: 'Visit past versions:',
+                pastVersions: [
+                    { item: '2016', itemUrl: '#' },
+                    { item: '2015', itemUrl: '#' },
+                    { item: '2014', itemUrl: '#' },
+                    { item: '2013', itemUrl: '#' }
                 ],
             },
             {
-                "src": "http://placehold.it/544x360",
-                "srcAlt": "dConstruct",
-                "eventCardContext": "May 24-26 2017  laban, london",
-                "eventCardSrc": "https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/leading-design-logo.svg",
-                "eventCardAlt": "dConstruct",
-                "eventCardUrl": "",
-                "eventCardLink": "dConstruct",
-                "eventCardDescription": "<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>",
-                "eventCardListTitle": "Visit past versions:"
+                src: 'http://placehold.it/544x360',
+                srcAlt: 'dConstruct',
+                eventCardContext: 'May 24-26 2017  laban, london',
+                eventCardSrc: 'https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/leading-design-logo.svg',
+                eventCardAlt: 'dConstruct',
+                eventCardUrl: '',
+                eventCardLink: 'dConstruct',
+                eventCardDescription: '<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>',
+                eventCardListTitle: 'Visit past versions:'
             },
             {
-                "src": "http://placehold.it/544x360",
-                "srcAlt": "Leading Design",
-                "eventCardContext": "May 24-26 2017  laban, london",
-                "eventCardSrc": "https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/ampersand-logo-white.svg",
-                "eventCardAlt": "Leading Design",
-                "eventCardUrl": "",
-                "eventCardLink": "Visit Leading Design",
-                "eventCardDescription": "<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>",
-                "eventCardListTitle": "Visit past versions:",
+                src: 'http://placehold.it/544x360',
+                srcAlt: 'Leading Design',
+                eventCardContext: 'May 24-26 2017  laban, london',
+                eventCardSrc: 'https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/ampersand-logo-white.svg',
+                eventCardAlt: 'Leading Design',
+                eventCardUrl: '',
+                eventCardLink: 'Visit Leading Design',
+                eventCardDescription: '<p>We bring engaging industry leaders from around the world to present a combination of inspirational talks and lively, hands-on workshops.</p>',
+                eventCardListTitle: 'Visit past versions:',
             }
         ],
 
         /*Footer*/
-        "footerLinks": "@site-footer.footerLinks",
-        "socialLinks": "@site-footer.socialLinks",
-        "inputGroupLabel": "@site-footer.inputGroupLabel",
-        "inputGroupSubmit": "@site-footer.inputGroupSubmit",
-        "inputModifier": '--combo',
-        "siteFooterSmall": "@site-footer.siteFooterSmall"
+        footerLinks: '@site-footer.footerLinks',
+        socialLinks: '@site-footer.socialLinks',
+        inputGroupLabel: '@site-footer.inputGroupLabel',
+        inputGroupSubmit: '@site-footer.inputGroupSubmit',
+        inputModifier: '--combo',
+        siteFooterSmall: '@site-footer.siteFooterSmall'
     }
 };
