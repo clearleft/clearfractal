b8ef765151ff0589f3723e9e97bb1c9c23ab8b5b
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-30 10:03:28 +0000

    Add Event Details page

---
+++ components/02-units/info-section/info-section.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    context: {
+        infoSectionHeading: 'Digital',
+        infoSectionContent: '<p>We’re here to see you through changes in your digital presence and practices.</p>'
+    },
+    variants: [
+        {
+            name: 'presents',
+            context: {
+                infoSectionModifiers: 'small'
+            }
+        }
+    ]
+};
