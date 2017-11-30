a5f180be42b2309219fa07f258da4ae08bf481aa
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-30 10:02:54 +0000

    Add variants and tweaks to exisiting styles

--- components/02-units/image-block/image-block.config.js
+++ components/02-units/image-block/image-block.config.js
@@ -1,25 +1,34 @@
 'use strict';
 module.exports = {
-    "status": "ready",
-    "context": {
-        "imageBlockSrc": "/assets/img/work/ch4/ch4-team.jpg",
-        "imageBlockAlt": "Channel 4 Team",
-        "imageBlockCaption": "The channel 4 news team. Image courtesy of ITN News"
+    status: 'ready',
+    context: {
+        imageBlockSrc: '/assets/img/work/ch4/ch4-team.jpg',
+        imageBlockAlt: 'Channel 4 Team',
+        imageBlockCaption: 'The channel 4 news team. Image courtesy of ITN News'
     },
-    "variants": [{
-        "name": "Right caption",
-        "context": {
-            "imageBlockSrc": "/assets/img/work/ch4/sketching-lo-hi.jpg",
-            "imageBlockModifier": "--rightCaption",
-            "imageBlockCaption": "Sketching from lo-fi to hi-fi"
+    variants: [{
+            name: 'Right caption',
+            context: {
+                imageBlockSrc: '/assets/img/work/ch4/sketching-lo-hi.jpg',
+                imageBlockModifier: '--rightCaption',
+                imageBlockCaption: 'Sketching from lo-fi to hi-fi'
+            }
+        },
+        {
+            name: 'Short',
+            context: {
+                imageBlockSrc: '/assets/img/work/ch4/ch4-team.png',
+                imageBlockModifier: '--short',
+                imageBlockCaption: false
+            }
+        },
+        {
+            name: 'first',
+            context: {
+                imageBlockSrc: 'https://s3-eu-west-1.amazonaws.com/clearleft-v5/uploads/Jan-Chipchase.jpg?mtime=20171007175433',
+                imageBlockModifier: '--first',
+                imageBlockCaption: false
+            }
         }
-    },
-    {
-        "name": "Short",
-        "context": {
-            "imageBlockSrc": "/assets/img/work/ch4/ch4-team.png",
-            "imageBlockModifier": "--short",
-            "imageBlockCaption": false
-        }
-    }]
+    ]
 }
