2f6c4b1d2be41337b323c7ce84e99749a8b4c7da
Cassie Evans <cassie.evans@clearleft.com>
2019-10-07 17:03:33 +0100

    Add announcement styles

---
+++ components/04-blocks/announcement/announcement.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    status: 'ready',
+    context: {
+        announcementHighlight: 'out now',
+        announcementTitle: 'Design Effectiveness Report 2019',
+        announcementDescription: 'We surveyed designers from hundreds of organisations to find out how design is currently used and understood in their workplaces. ',
+        megaLinkURL: '#',
+        megaLinkText: 'Download the report'
+    }
+}
