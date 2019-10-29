bde7bf66a1186f35eb1269119128e89784b5cfbe
Cassie Evans <cassie.evans@clearleft.com>
2019-10-29 14:38:01 +0000

    Add number component

---
+++ components/03-units/numbers/numbers.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    status: "ready",
+    preview: "@preview-scroll",
+    collated: true,
+    context: {},
+    notes: "These are some **notes** about the component",
+    variants: [
+        {
+            name: "default",
+            context: {
+                number: "1"
+            }
+        },
+        {
+            name: "two",
+            context: {
+                number: "2"
+            }
+        },
+        {
+            name: "three",
+            context: {
+                number: "3"
+            }
+        }
+    ]
+};
