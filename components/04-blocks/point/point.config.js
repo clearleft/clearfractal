25333a6a38cf22c11f8d547247a8b0b827e7e067
Cassie Evans <cassie.evans@clearleft.com>
2019-10-30 12:04:30 +0000

    Add point component

--- components/04-blocks/point/point.config.js
+++ components/04-blocks/point/point.config.js
@@ -6,36 +6,36 @@ module.exports = {
         {
             name: "default",
             context: {
-                pointHeading: "Open the conversation.",
-                pointBody: `
-            <p> Ask us anything. From basic questions to complex queries about your approach to strategy, research, content and design, Dan is ready to talk to you on + 44(0) 845 838 6163 or <a href ="#"> get in touch </a></p>`
+                pointHeading: "No number",
+                pointBody:
+                    "The physical working environment supports collaborative design activities"
             }
         },
         {
             name: "one",
             context: {
                 pointModifiers: "1",
-                pointHeading: "Open the conversation.",
-                pointBody: `
-            <p> Ask us anything. From basic questions to complex queries about your approach to strategy, research, content and design, Dan is ready to talk to you on + 44(0) 845 838 6163 or <a href ="#"> get in touch </a></p>`
+                pointHeading: "Empowerment",
+                pointBody:
+                    "The physical working environment supports collaborative design activities"
             }
         },
         {
             name: "two",
             context: {
                 pointModifiers: "2",
-                pointHeading: "Open the conversation.",
-                pointBody: `
-            <p> Ask us anything. From basic questions to complex queries about your approach to strategy, research, content and design, Dan is ready to talk to you on + 44(0) 845 838 6163 or <a href ="#"> get in touch </a></p>`
+                pointHeading: "Environment",
+                pointBody:
+                    "The physical working environment supports collaborative design activities"
             }
         },
         {
             name: "three",
             context: {
                 pointModifiers: "3",
-                pointHeading: "Open the conversation.",
-                pointBody: `
-            <p> Ask us anything. From basic questions to complex queries about your approach to strategy, research, content and design, Dan is ready to talk to you on + 44(0) 845 838 6163 or <a href ="#"> get in touch </a></p>`
+                pointHeading: "Research",
+                pointBody:
+                    "The physical working environment supports collaborative design activities"
             }
         }
     ]
