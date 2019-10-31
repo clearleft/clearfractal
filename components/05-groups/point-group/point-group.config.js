e9d96934c7d8a4f7aceca9d1f2763499b7fc8e9e
Cassie Evans <cassie.evans@clearleft.com>
2019-10-31 10:04:33 +0000

    Add point group component

---
+++ components/05-groups/point-group/point-group.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    "status": "wip",
+    "context": {
+        "pointItems": [{
+            pointModifiers: "1",
+            pointHeading: "Empowerment",
+            pointBody: "The physical working environment supports collaborative design activities"
+        },
+        {
+            pointModifiers: "2",
+            pointHeading: "Empowerment",
+            pointBody: "The physical working environment supports collaborative design activities"
+        },
+        {
+            pointModifiers: "3",
+            pointHeading: "Empowerment",
+            pointBody: "The physical working environment supports collaborative design activities"
+        }]
+    }
+}
