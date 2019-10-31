c957fe2acc78a4ffc486dfed0a4f486b20e4d253
Cassie Evans <cassie.evans@clearleft.com>
2019-10-31 11:09:16 +0000

    Build design effectiveness page

--- components/09-pages/design-effectiveness/design-effectiveness.config.js
+++ components/09-pages/design-effectiveness/design-effectiveness.config.js
@@ -1,38 +1,57 @@
-const faker = require('faker');
-
-// let quotes      = [];
-
-// for (var i = 0; i < 6; i++) {
-//     quotes.push({
-//         quote: faker.lorem.sentence(12),
-//         authorImage: "/assets/img/authors/sm/edv.png",
-//         authorName: "Ellen DeVries",
-//         datePosted: 'Sep 21'
-//     });
-// }
+const faker = require("faker");
 
 module.exports = {
-    "status": "wip",
-    "context": {
+    status: "wip",
+    context: {
         /*Header*/
-        "primaryNavlinks": "@primary-navigation.primaryNavlinks",
-        "primaryNavModifiers": ["--lapis-lazuli-blue", "--inverted"],
-        "logoModifiers": ["--lapis-lazuli-blue", "--inverted"],
+        primaryNavlinks: "@primary-navigation.primaryNavlinks",
+        primaryNavModifiers: ["--lapis-lazuli-blue", "--inverted"],
+        logoModifiers: ["--lapis-lazuli-blue", "--inverted"],
 
         /* Home Splash */
-        "homeHeader": "We help design leaders get the most from their products, services &amp; teams.",
-        "homeSubheader": "Find out <a href=\"#\">who we are</a> or <a href=\"#\">how we can help</a>",
+        homeHeader:
+            "We help design leaders get the most from their products, services &amp; teams.",
+        homeSubheader:
+            'Find out <a href="#">who we are</a> or <a href="#">how we can help</a>',
 
+        /*Point group */
+        pointItems: [
+            {
+                pointModifiers: "1",
+                pointHeading: "Empowerment",
+                pointBody:
+                    "The physical working environment supports collaborative design activities"
+            },
+            {
+                pointModifiers: "2",
+                pointHeading: "Empowerment",
+                pointBody:
+                    "The physical working environment supports collaborative design activities"
+            },
+            {
+                pointModifiers: "3",
+                pointHeading: "Empowerment",
+                pointBody:
+                    "The physical working environment supports collaborative design activities"
+            }
+        ],
+
+        /* Quote */
+        quoteModifiers: "standalone",
+        quoteText:
+            "Clearleft listened, shared, and designed alongside us. Enabling us to evolve from crawling, walking, and finally running at a great pace in the development of our design practice",
+        quoteAuthor: "Don Norman",
+        quoteAuthorRole: "Speaker at UX London 2009",
 
         /*Footer*/
-        "footerLinks": "@site-footer.footerLinks",
-        "socialLinks": "@site-footer.socialLinks",
-        "inputGroupLabel": "@site-footer.inputGroupLabel",
-        "inputGroupSubmit": "@site-footer.inputGroupSubmit",
-        "inputGroupID": "@site-footer.inputGroupID",
-        "inputGroupPlaceholder": "@site-footer.inputGroupPlaceholder",
-        "inputGroupMicrocopy": "@site-footer.inputGroupMicrocopy",
-        "inputGroupModifiers": "@site-footer.inputGroupModifiers",
-        "siteFooterSmall": "@site-footer.siteFooterSmall"
+        footerLinks: "@site-footer.footerLinks",
+        socialLinks: "@site-footer.socialLinks",
+        inputGroupLabel: "@site-footer.inputGroupLabel",
+        inputGroupSubmit: "@site-footer.inputGroupSubmit",
+        inputGroupID: "@site-footer.inputGroupID",
+        inputGroupPlaceholder: "@site-footer.inputGroupPlaceholder",
+        inputGroupMicrocopy: "@site-footer.inputGroupMicrocopy",
+        inputGroupModifiers: "@site-footer.inputGroupModifiers",
+        siteFooterSmall: "@site-footer.siteFooterSmall"
     }
-}
+};
