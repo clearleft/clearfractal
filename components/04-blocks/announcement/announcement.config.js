95194f140c7a8f12c4a644ec29e9bf1d2b3872d2
Trys Mudford <trys@clearleft.com>
2020-05-07 14:55:42 +0100

    Add inline image to announcement

--- components/04-blocks/announcement/announcement.config.js
+++ components/04-blocks/announcement/announcement.config.js
@@ -5,8 +5,38 @@ module.exports = {
         announcementHighlight: 'out now',
         announcementTitle: 'Design Effectiveness Report 2019',
         announcementDescription: 'We surveyed designers from hundreds of organisations to find out how design is currently used and understood in their workplaces. ',
-        announcementImage: '',
+        announcementImage: '/assets/img/illustrations/handsup.png',
         megaLinkURL: '#',
-        megaLinkText: 'Download the report'
-    }
+        megaLinkText: 'Download the report',
+        inlineImage: false
+    },
+    variants: [
+        {
+            name: 'No highlight',
+            context: {
+                announcementHighlight: ''
+            }
+        },
+        {
+            name: 'No image',
+            context: {
+                announcementImage: ''
+            }
+        },
+        {
+            name: 'Inline image',
+            context: {
+                inlineImage: true,
+                announcementImage: '/assets/img/team-gallery/sketching.jpg'
+            }
+        },
+        {
+            name: 'Inline image - No highlight',
+            context: {
+                announcementHighlight: '',
+                inlineImage: true,
+                announcementImage: '/assets/img/team-gallery/sketching.jpg'
+            }
+        }
+    ],
 }
