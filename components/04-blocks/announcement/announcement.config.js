aeb7e19f759ec96e65a11d58ffaed31a98823cb7
Trys Mudford <trys@clearleft.com>
2020-05-07 11:22:56 +0100

    Announcment image option

--- components/04-blocks/announcement/announcement.config.js
+++ components/04-blocks/announcement/announcement.config.js
@@ -5,6 +5,7 @@ module.exports = {
         announcementHighlight: 'out now',
         announcementTitle: 'Design Effectiveness Report 2019',
         announcementDescription: 'We surveyed designers from hundreds of organisations to find out how design is currently used and understood in their workplaces. ',
+        announcementImage: '',
         megaLinkURL: '#',
         megaLinkText: 'Download the report'
     }
